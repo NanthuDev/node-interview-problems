51c464a844344f6c2bba15703cd74fbd811d06b4
Nantha <nanthu1593@gmail.com>
2024-05-31 23:22:34 +0800

    Update arrayReduce.js

--- arrayReduce.js
+++ arrayReduce.js
@@ -10,16 +10,27 @@ const sumArray = (arr)=>{
 
  
 //2.Flatten a nested array
-
 const flattenArray = (arr)=>{
   return arr.reduce((flatten, toBeflat)=>  flatten.concat(Array.isArray(toBeflat)?flattenArray(toBeflat):toBeflat),[]) 
    
 }
-
 console.log(flattenArray([1, [2, [3, 4], 5], 6])); // Output: [1, 2, 3, 4, 5, 6]
 
 
+
+
 //3.Count occurences of elements
+const countOccurrences = (arr)=>{
+  return arr.reduce((countMap,item)=>{
+    countMap[item] = (countMap[item] | 0) + 1
+    return countMap;
+  },{})
+
+}
+
+// Example usage:
+console.log(countOccurrences(['apple', 'banana', 'apple', 'orange', 'banana', 'apple']));
+// Output: { apple: 3, banana: 2, orange: 1 }
 
 
 
